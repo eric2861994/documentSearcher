da5ff3edd23e62e9d27b183b6508e5d51b97e414
Kevin Yudi Utama <kevin.kayu@gmail.com>
2015-10-09 16:46:59 +0700

    Erase System.out.println in test

--- backend/src/main/test/stbi/indexer/ModifiedInvertedIndexerTest.java
+++ backend/src/main/test/stbi/indexer/ModifiedInvertedIndexerTest.java
@@ -51,7 +51,6 @@ public class ModifiedInvertedIndexerTest {
         for (RawDocument oneDocument : documents) {
             int docIdx = getDocumentIndex(oneDocument.getId(), testDocuments);
             if (docIdx != -1) {
-                System.out.println(oneDocument.getTitle()+" n-n "+testDocuments[docIdx].getTitle());
                 Assert.assertEquals(oneDocument, testDocuments[docIdx]);
             }
         }
