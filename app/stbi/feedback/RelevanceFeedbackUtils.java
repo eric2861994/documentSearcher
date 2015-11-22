2d2fcd226ac05b20808a650b21108451dd6d8ffb
Kevin Yudi Utama <kevin.kayu@gmail.com>
2015-11-22 22:14:38 +0700

    Fix Rochio,Ide and Dehai

--- app/stbi/feedback/RelevanceFeedbackUtils.java
+++ app/stbi/feedback/RelevanceFeedbackUtils.java
@@ -12,41 +12,76 @@ import java.util.*;
 public class RelevanceFeedbackUtils {
 
     public static Map<Term,Double> rochioQueryReweighting(Map<Term,Double> oldQueryVector,
-                                           List<RawDocument> relevantDocuments,
-                                           List<RawDocument> unrelevantDocuments,
-                                           boolean doExapnsion) {
-            return constructNewQuery(oldQueryVector,
-                    relevantDocuments,
-                    unrelevantDocuments,
-                    true,
-                    doExapnsion);
+                                           List<Map<Term,Double>> relevantDocuments,
+                                           List<Map<Term,Double>> unrelevantDocuments,
+                                           boolean doExpansion) {
+
+        Map<Term,Double> relevantDocumentVector = addTermVector(relevantDocuments);
+        Map<Term,Double> unrelevantDocumentVector = addTermVector(unrelevantDocuments);
+
+        int nRelevantDocuments = relevantDocuments.size();
+        int nUnrelevantDocuments = unrelevantDocuments.size();
+        for (Map.Entry<Term,Double> entry : relevantDocumentVector.entrySet()) {
+            Term key = entry.getKey();
+            relevantDocumentVector.put(key,entry.getValue()/nRelevantDocuments);
+        }
+
+        for (Map.Entry<Term,Double> entry : unrelevantDocumentVector.entrySet()) {
+            Term key = entry.getKey();
+            unrelevantDocumentVector.put(key,entry.getValue()/nUnrelevantDocuments);
+        }
+
+        return computeTermReweighting(oldQueryVector,
+                relevantDocumentVector,
+                unrelevantDocumentVector,
+                doExpansion);
+
     }
 
     public static Map<Term,Double> ideQueryReweighting(Map<Term,Double> oldQueryVector,
-                                                        List<RawDocument> relevantDocuments,
-                                                        List<RawDocument> unrelevantDocuments,
-                                                        boolean doExapnsion) {
-        return constructNewQuery(oldQueryVector,
-                relevantDocuments,
-                unrelevantDocuments,
-                false,
-                doExapnsion);
+                                                        List<Map<Term,Double>> relevantDocuments,
+                                                        List<Map<Term,Double>> unrelevantDocuments,
+                                                        boolean doExpansion) {
+        Map<Term,Double> relevantDocumentVector = addTermVector(relevantDocuments);
+        Map<Term,Double> unrelevantDocumentVector = addTermVector(unrelevantDocuments);
+        return computeTermReweighting(oldQueryVector,
+                relevantDocumentVector,
+                unrelevantDocumentVector,
+                doExpansion);
     }
 
     public static Map<Term,Double> deHaiQueryReweighting(Map<Term,Double> oldQueryVector,
-                                                         List<RawDocument> relevantDocuments,
-                                                         RawDocument unrelevantDocument,
-                                                         boolean doExapnsion) {
-        List<RawDocument> unrelevantDocuments = new ArrayList<RawDocument>();
-        unrelevantDocuments.add(unrelevantDocument);
-        return constructNewQuery(oldQueryVector,
-                relevantDocuments,
-                unrelevantDocuments,
-                false,
-                doExapnsion);
+                                                         List<Map<Term,Double>> relevantDocuments,
+                                                         Map<Term,Double> unrelevantDocument,
+                                                         boolean doExpansion) {
+        Map<Term,Double> relevantDocumentVector = addTermVector(relevantDocuments);
+        return computeTermReweighting(oldQueryVector,
+                relevantDocumentVector,
+                unrelevantDocument,
+                doExpansion);
     }
 
-    public static Map<Term,Double> constructNewQuery(Map<Term,Double> oldQueryVector,
+    private static Map<Term,Double> addTermVector(List<Map<Term,Double>> termVectors) {
+        Map<Term,Double> resultVector = new HashMap<>();
+
+        Set<Term> resultTerms = new HashSet<Term>();
+        for (Map<Term,Double> termVector : termVectors) {
+            resultTerms.addAll(termVector.keySet());
+        }
+
+        Iterator<Term> iterator = resultTerms.iterator();
+        while(iterator.hasNext()) {
+            Term term = iterator.next();
+            Double weight = 0.0;
+            for (Map<Term,Double> termVector : termVectors) {
+                if (termVector.containsKey(term)) weight += termVector.get(term);
+            }
+            resultVector.put(term,weight);
+        }
+        return resultVector;
+    }
+
+    private static Map<Term,Double> constructNewQuery(Map<Term,Double> oldQueryVector,
                                                          List<RawDocument> relevantDocuments,
                                                          List<RawDocument> unrelevantDocuments,
                                                         boolean doDivide,
@@ -63,7 +98,7 @@ public class RelevanceFeedbackUtils {
                 relevantDocumentVector.put(key,entry.getValue()/nRelevantDocuments);
             }
 
-            for (Map.Entry<Term,Double> entry : relevantDocumentVector.entrySet()) {
+            for (Map.Entry<Term,Double> entry : unrelevantDocumentVector.entrySet()) {
                 Term key = entry.getKey();
                 unrelevantDocumentVector.put(key,entry.getValue()/nUnrelevantDocuments);
             }
